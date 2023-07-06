543cbb418ecfa1a0c644185e6ab8f396ba3fea3a
ridwannhd0705 <ridwannur07@student.uns.ac.id>
2023-07-06 22:33:58 +0700

    'minor'

--- routes/media.js
+++ routes/media.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
 const fs = require('fs');
